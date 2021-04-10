734d66b3bf3528ace0af37a687efbe38ed3e2552
Ibmug <rosv58@gmail.com>
2021-04-10 12:27:33 -0500

    Smoothing?

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const passport = require("./config/passport");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 var fs = require("fs");
-// var morgan = require("morgan");
 var path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,14 +20,6 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
-///Set up logging:
-// var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
-// 	flags: "a",
-// });
-
-// setup the logger
-// app.use(morgan("combined", { stream: accessLogStream }));
 
 //Set a mongo connection.
 const uri =
@@ -49,9 +40,6 @@ if (process.env.NODE_ENV === "production") {
 }
 // Add routes, both API and view
 app.use(routes);
-
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/doctorsDB");
 
 // Start the API server
 app.listen(PORT, function () {
