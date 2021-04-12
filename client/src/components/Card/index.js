d0b1259c71a8859fa9dc07efd24593d4e63f3052
Ibmug <rosv58@gmail.com>
2021-04-11 21:49:46 -0500

    Changing scroll, must add a link to sign in maybe...

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,16 +1,22 @@
 import Kard from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+
 import "./style.css";
 
 const Card = (props) => {
 	return (
 		<>
-			<Kard className="kard">
+			<Kard border={props.border} className="kard">
 				<Kard.Img variant="top" src={props.img} />
 				<Kard.Body>
 					<Kard.Title>{props.title}</Kard.Title>
 					<Kard.Text>{props.text}</Kard.Text>
-					<Button variant="primary" to={props.path}>
+					<Button
+						onClick={(e) => {
+							e.preventDefault();
+							window.open(props.path, "_blank");
+						}}
+					>
 						Repo or page
 					</Button>
 				</Kard.Body>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,56 +1,67 @@
-import React from "react";
+import Card from "../components/Card";
+import Col from "../components/Col";
+import Row from "../components/Row";
 import Container from "../components/Container";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Hero from "../components/Hero";
 import backgroundImage from "../utils/images/banner.png";
 
-import {
-	FaGithub,
-	FaInstagram,
-	FaLinkedin,
-	FaSoundcloud,
-	FaTwitch,
-} from "react-icons/fa";
-
-import SignForm from "../components/SignForm";
-import API from "../utils/API";
-
-const mediaObj = [
+const images = [
 	{
-		text: "Streams",
-		url: "https://twitch.tv/ibmug",
-		icon: FaTwitch,
+		path: "https://ibmug.github.io/juegoma_dev/",
+		img: "Unity_Game.JPG",
+		title: "Unity Game",
+		text: "Still under construction...",
+		border: "warning",
 	},
 	{
-		text: "Music",
-		url: "https://soundcloud.com/ibmug",
-		icon: <FaSoundcloud />,
+		path: "https://ibmug-emed.herokuapp.com/",
+		img: "emed.JPG",
+		title: "eMed",
+		text: "Interface for Clinics",
+		border: "success",
 	},
 	{
-		text: "Instagram",
-		url: "https://www.instagram.com/ibmug/",
-		icon: <FaInstagram />,
+		path: "https://ibmug.github.io/proyecto_1/",
+		img: "buscaCovid.jpg",
+		title: "Covid Searcher in Mexico",
+		text: "This was done for a learning proyect.",
+		border: "info",
 	},
 	{
-		text: "Git",
-		url: "https://github.com/ibmug",
-		icon: <FaGithub />,
+		path: "https://imbug-expenses.herokuapp.com/",
+		img: "Expense_Tracker.JPG",
+		title: "Expense Tracker",
+		text: "Simple yet effective Tracker",
+		border: "dark",
 	},
 	{
-		text: "LinkedIn",
-		url: "https://linkedin.com/in/rodrigosv8",
-		icon: <FaLinkedin />,
+		path: "https://ibmug-notetaker.herokuapp.com/",
+		img: "noteTaker.JPG",
+		title: "Note taker!",
+		text: "Easy post to track notes...",
+	},
+	{
+		path: "https://medidor-ejercicio.herokuapp.com/",
+		img: "Fitness_track_poster.JPG",
+		title: "Fitness Bragger",
+		text: "Easy post to DB form",
+	},
+	{
+		path: "https://ibmug.github.io/simpleDaySchedule/",
+		img: "simpleSchedule.JPG",
+		title: "Simple Scheduler",
+		text: "A simple scheduler, needs a lot of refining..",
 	},
 ];
 
-const mediaItems = mediaObj.map((media) => (
-	<li>
-		<a href={media.url} target="_blank">
-			{" "}
-			{media.text}
-			{media.icon}
-		</a>
-	</li>
+const workItems = images.map((image) => (
+	<Card
+		path={image.path}
+		img={"./assets/img/" + image.img}
+		title={image.title}
+		text={image.text}
+		border={image.border ? image.border : "secondary"}
+	/>
 ));
 
 const Home = (props) => {
@@ -58,11 +69,11 @@ const Home = (props) => {
 		<>
 			<Hero backgroundImage={backgroundImage}>
 				<h1>Ibmug</h1>
-				<h2>Create an account to see some more!</h2>
+				<h2>Create an account to see additional features!</h2>
 			</Hero>
 			<Container>
-				<div>Find me on the following platforms:</div>
-				<ul>{mediaItems}</ul>
+				<p>Behold!...My Stuff...</p>
+				<Row>{workItems}</Row>
 			</Container>
 		</>
 	);
